1de0e47d02a193f75f541cf2230cef62b7c79651
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-06-24 18:52:06 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Slotkin_PropertiesDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student molly = new Student("Molly Cat", 1234);
+
+            Console.WriteLine("molly's Name: {0}", molly.Name);
+            Console.WriteLine("molly's IdNumber: {0}", molly.IdNumber);
+            molly.Score1 = 100;
+            molly.Score2 = 80;
+            Console.WriteLine("molly's Average Score: {0}", molly.GetAverageScore());
+            Console.WriteLine("molly's High Score: {0}", molly.HighScore);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slotkin_PropertiesDemo1
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int IdNumber { get; set; }
+        public double AverageScore { get; private set; }
+        
+        private double averageScore;
+
+        public Student(string newName, int newIdNumber)
+        {
+            this.Name = newName;
+            this.IdNumber = newIdNumber;
+        }
+
+        private int score1;
+        public int Score1
+        {
+            get { return score1; }
+            set { score1 = value; UpdateAverageScore(); }
+        }
+        private int score2;
+
+        public int Score2
+        {
+            get { return score2; }
+            set { score2 = value; UpdateAverageScore(); }
+        }
+
+        public double HighScore
+        {
+            get
+            {
+                if (score1 > score2)
+                {
+                    return score1;
+                }
+                else
+                {
+                    return score2;
+                }
+            }
+        }
+
+        public double GetAverageScore()
+        {
+            return this.averageScore;
+        }
+
+        private void UpdateAverageScore()
+        {
+            this.averageScore = (score1 + score2) / 2;
+        }
+    }
+}
